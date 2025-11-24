e8b05c6713c36ed62636de7c3fc3e350b4716bf0
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-11-24 12:35:46 -0300

    test: :white_check_mark: login controller tests passing

--- backend/src/controllers/auth-controller.test.ts
+++ backend/src/controllers/auth-controller.test.ts
@@ -41,14 +41,60 @@ describe('auth-controller', () => {
         next as NextFunction
       );
       expect(registerSchema.parse).toHaveBeenCalledWith(reqBody);
-      
+
       expect(deps.authService.register).toHaveBeenCalledWith({
         email: 'user@email.com',
         password: 'secret123',
       });
       expect(res.status).toHaveBeenCalledWith(201);
       expect(res.send).toHaveBeenCalledWith('User registered.');
-      expect(next).not.toHaveBeenCalled()
+      expect(next).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('login', () => {
+    it('calls authService.login', async () => {
+      const reqBody = {
+        email: 'user@email.com',
+        password: 'secret123',
+      };
+      const req: Partial<Request> = {
+        body: reqBody,
+      };
+      const res: Partial<Response> = {
+        status: vi.fn().mockReturnThis(),
+        json: vi.fn(),
+      };
+      const next: Partial<NextFunction> = vi.fn();
+      const safeUser = {
+        id: '1',
+        email: 'user@email.com',
+        createdAt: '',
+        updatedAt: ''
+      };
+      vi.mocked(registerSchema.parse).mockReturnValue(reqBody);
+      const deps = {
+        authService: {
+          register: vi.fn(),
+          login: vi.fn().mockResolvedValue(safeUser),
+        },
+      };
+
+      const controller = authController(deps);
+      await controller.login(
+        req as Request,
+        res as Response,
+        next as NextFunction
+      );
+      expect(registerSchema.parse).toHaveBeenCalledWith(reqBody);
+
+      expect(deps.authService.login).toHaveBeenCalledWith({
+        email: 'user@email.com',
+        password: 'secret123'
+      });
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith(safeUser);
+      expect(next).not.toHaveBeenCalled();
     });
   });
 });
--- backend/src/controllers/auth-controller.ts
+++ backend/src/controllers/auth-controller.ts
@@ -1,6 +1,9 @@
 import type { ControllerDeps } from '../types/auth-types.js';
 import type { Request, Response, NextFunction } from 'express';
 import { registerSchema } from '../types/auth-types.js';
+import { createToken, verifyToken } from '../utils/auth.js';
+
+const isProd = process.env.NODE_ENV === 'production';
 
 export const authController = (deps: ControllerDeps) => ({
   register: async (req: Request, res: Response, next: NextFunction) => {
@@ -13,5 +16,20 @@ export const authController = (deps: ControllerDeps) => ({
     }
   },
 
-  login: async (req: Request, res: Response, next: NextFunction) => {},
+  login: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = registerSchema.parse(req.body);
+      const result = await deps.authService.login(data);
+      const token = createToken({ id: result.id, email: result.email });
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: isProd,
+        sameSite: isProd ? 'none' : 'lax',
+        maxAge: 24 * 60 * 60 * 1000,
+      });
+      res.status(200).json(result);
+    } catch (error) {
+      next(error);
+    }
+  },
 });
