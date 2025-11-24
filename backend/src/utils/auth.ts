051dcfd457fc6be4b78f3422c3c2f5f11093c18d
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-11-24 12:32:30 -0300

    add jwt functions

--- backend/src/utils/auth.ts
+++ backend/src/utils/auth.ts
@@ -1,4 +1,7 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { secret } from '../middlewares/auth.js';
+import type { TokenPayload } from '../types/auth-types.js';
 
 export async function hash(password: string): Promise<string> {
   return await bcrypt.hash(password, 10);
@@ -9,4 +12,12 @@ export async function compare(
   hash: string
 ): Promise<boolean> {
   return await bcrypt.compare(password, hash);
+}
+
+export function createToken(data: TokenPayload) {
+  return jwt.sign(data, secret);
+}
+
+export function verifyToken (token: string) {
+  return jwt.verify(token, secret)
 }
