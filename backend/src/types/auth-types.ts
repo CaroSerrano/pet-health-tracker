45d92a5b11e2001a2af8e974788ab3d620e554d1
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-11-24 12:32:09 -0300

    update auth types

--- backend/src/types/auth-types.ts
+++ backend/src/types/auth-types.ts
@@ -1,8 +1,9 @@
 import { z } from 'zod';
+import type { User } from '../generated/prisma/client.js';
 
 export interface IAuthService {
   register: (data: RegisterPayload) => Promise<void>;
-  login: (...args: any) => Promise<any>;
+  login: (...args: any) => Promise<SafeUser>;
 }
 export interface ControllerDeps {
   authService: IAuthService;
@@ -19,3 +20,20 @@ export const registerSchema = z.object({
 });
 
 export type RegisterSchema = z.infer<typeof registerSchema>;
+
+export type SafeUser = Omit<User, 'passwordHash'>;
+
+export interface TokenPayload {
+  id: string;
+  email: string;
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: TokenPayload | undefined;
+    }
+  }
+}
+
+export {};
