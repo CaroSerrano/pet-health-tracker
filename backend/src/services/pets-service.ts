4549975ba2ad8178daa498fb5f386d5ca1565053
CERP <carloscer27r@gmail.com>
2025-11-24 21:12:10 -0300

    Se asigna al prisma/client local

--- backend/src/services/pets-service.ts
+++ backend/src/services/pets-service.ts
@@ -1,4 +1,4 @@
-import type { PrismaClient } from '@prisma/client';
+import type { PrismaClient } from '../generated/prisma/client.js';
 import { error } from 'console';
 
 export class PetService {
@@ -20,42 +20,14 @@ export class PetService {
   }: {
     userId: string; 
     name: string;
-    species?: string;
-    breed?: string;
-    age?: number;
-    weight?: number;
-    photoUrl?: string;
+    species: string;
+    breed: string;
+    age: number;
+    weight: number;
+    photoUrl: string;
     
-  }) {
+  }) {    
     
-    //realizar todas las validaciones
-    if (!name)   { throw new Error ("El nombre es obligatorio"); }   
-
-    if (typeof name !== "string") {
-      throw new Error("El nombre debe ser  texto");
-    }
-
-    if (typeof species !== "string") {
-      throw new Error("La especie debe ser texto");
-    }
-
-    if (typeof breed !== "string") {
-      throw new Error("La raza debe ser texto");
-    }
-
-    if (typeof age !== "number" || age <= 0) {
-      throw new Error("La edad debe ser un número mayor que 0");
-    }
-
-    if (typeof weight !== "number" || weight <= 0) {
-      throw new Error("El peso debe ser un número mayor que 0");
-    }
-
-    if (photoUrl && typeof photoUrl !== "string") {
-      throw new Error("La foto debe ser una URL válida");
-    }
-
-
     return await this.prisma.pet.create({
       data: {
         userId,
